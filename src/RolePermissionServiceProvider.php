1ef0c9560943d0b8775e14c96c60cebfe31cf88f
Stephen Luck Enguio <stephenenguio1120@gmail.com>
2025-09-02 14:38:27 +0800

    chore: register config

--- src/RolePermissionServiceProvider.php
+++ src/RolePermissionServiceProvider.php
@@ -10,16 +10,17 @@ class RolePermissionServiceProvider extends ServiceProvider
     {
         // Merge package config with app config
         $this->mergeConfigFrom(
-            __DIR__ . '/config/role-permission.php',
+            __DIR__.'/../../config/role-permission.php',
             'role-permission'
         );
     }
 
     public function boot()
     {
-        // Allow publishing of config so project can override
+          // Publish config
         $this->publishes([
-            __DIR__ . '/config/role-permission.php' => config_path('role-permission.php'),
+            __DIR__.'/../../config/role-permission.php' => config_path('role-permission.php'),
         ], 'config');
+
     }
 }
